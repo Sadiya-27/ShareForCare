093a46d1d4035336e66223093ef145fa4091e162
Sadiya Fatima Khwaja <104360019+Sadiya-27@users.noreply.github.com>
2025-11-16 21:06:35 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
   {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
+    },
+
     ignores: [
       "node_modules/**",
       ".next/**",
